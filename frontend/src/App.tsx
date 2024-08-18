044679956d7f8b05f982c385bb77ba50540420ff
Ugur Kartal <kartalugur@gmail.com>
2024-08-18 16:57:31 +0200

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'semantic-ui-css/semantic.min.css'
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
